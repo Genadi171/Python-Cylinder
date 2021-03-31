33323fb48ff638d2839920d4ef617958018983d1
Genadi171 <73273960+Genadi171@users.noreply.github.com>
2021-03-31 09:57:16 +0300

    Add files via upload

---
+++ cylinder.py
@@ -0,0 +1,7 @@
+import math
+r=float(input("Enter r of a cylinde"))
+h=float(input("Enter the Height of a cylinde"))
+s_area=2*math.pi*pow(r,2)*h
+volume=math.pi*pow(r,2)*h
+print("surface area of a cylinder wll be %.2f" %s_area)
+print("volume of a cylinder will be  %.2f" %volume)
